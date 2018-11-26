d5752f3ca55c0efbcddf757358ca8aca4fb26e34
arjun <ag870994@gmail.com>
2018-11-26 17:35:39 +0530

    added all the folders

---
+++ build/precache-manifest.0550c8c30c160c0fec62bc750407971b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "84287d09b8053c6fa598893b8910786a",
+    "url": "/static/media/logo.84287d09.svg"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b541460fbc61417c7496",
+    "url": "/static/js/main.b541460f.chunk.js"
+  },
+  {
+    "revision": "97c5e76f74bc7f05c891",
+    "url": "/static/js/1.97c5e76f.chunk.js"
+  },
+  {
+    "revision": "b541460fbc61417c7496",
+    "url": "/static/css/main.57e0f0a8.chunk.css"
+  },
+  {
+    "revision": "175075a255e77406bcdd13a2224c857f",
+    "url": "/index.html"
+  }
+];
